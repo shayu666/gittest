36aac9748e7ca8655f1c10cd334bfa6be45341a6
shayu666 <572338031@qq.com>
2020-08-07 10:39:17 +0800

    add hello world

---
+++ git/src/main/java/com/demo/git/test/testMehtod.java
@@ -0,0 +1,12 @@
+package com.demo.git.test;
+
+/**
+ * @author lls
+ * @create 2020-08-07 10:37
+ */
+
+public class testMehtod {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
